cb4cef0e8a68fb9e286b31b02d5546c50c48e7b8
Jennifer Osato <st.jennyandy@gmail.com>
2024-11-24 04:48:40 +0100

    Added the chat UI

---
+++ src/components/LogoScroll.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import cu from "../assets/images/cu-logo.png";
+import miva from "../assets/images/miva logo.png";
+import nexford from "../assets/images/nexford.png";
+import quantic from "../assets/images/quantic.png";
+import pau from "../assets/images/pau.png";
+import noun from "../assets/images/noun.jpg";
+import nutm from "../assets/images/nutm.png";
+
+const logos = [cu, miva, nexford, quantic, pau, noun, nutm];
+
+const LogoScroll: React.FC = () => {
+  return (
+    <div className="relative overflow-hidden w-full">
+      <div className="flex gap-10 animate-marquee whitespace-nowrap w-[200%]">
+        {logos.map((logo, index) => (
+          <img
+            key={index}
+            src={logo}
+            alt={`Logo ${index + 1}`}
+            className="w-[10rem] object-contain"
+          />
+        ))}
+        {logos.map((logo, index) => (
+          <img
+            key={index + logos.length}
+            src={logo}
+            alt={`Logo ${index + 1}`}
+            className="w-[10rem] object-contain"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default LogoScroll;
